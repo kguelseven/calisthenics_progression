516df25ebf1b34bb44cd5c6b5371175ebc1d951d
Richi Sixt <richi.sixt@gmail.com>
2019-01-21 21:35:40 +0100

    add templates folder

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    # Mock User
+    user = {'username' : 'Richi'}
+    workouts = [
+        {
+            'athlet' : { 'username' : 'John'},
+            'title' : 'Workout Day 1'
+        },
+        {
+            'athlet' : { 'username' : 'Ali'},
+            'title' : 'Workout Day 2'
+        },
+
+    ]
+    return render_template('index.html', title='Home', user=user, workouts=workouts)
+
