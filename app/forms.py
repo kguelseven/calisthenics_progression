9722d7ef3da96f6fcb2dec408df6be265e33aba0
Richi Sixt <richi.sixt@gmail.com>
2019-01-23 07:59:17 +0100

    improving templates, installing SQLAlchemy  & Flask Migrate

--- app/forms.py
+++ app/forms.py
@@ -6,4 +6,4 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Submit', validators=[DataRequired()])
+    submit = SubmitField('Submit')
