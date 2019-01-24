ee870653219a0cda12626098cba1c472a72bf968
Richi Sixt <richi.sixt@gmail.com>
2019-01-24 06:50:54 +0100

    workout model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,22 @@
 from app import db
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    workouts = db.relationship('Workouts', backref='athlet', lazy='dynamic')
+    
 
     def __repr__(self):
         return '<user {}>'.format(self.username)
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), index=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<workout {}>'.format(self.title)
