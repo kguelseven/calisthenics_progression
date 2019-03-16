202356f62cbac7efed0bdb3cb3557c530ebcf77a
Richi Sixt <richi.sixt@gmail.com>
2019-03-16 00:41:03 +0000

    config pythonanywhere

--- project/_config.py
+++ project/_config.py
@@ -5,11 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 #main config
-DEBUG = True
+DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 MYSQL_CURSORCLASS = 'DictCursor'
 WORKOUTS_PER_PAGE = 10
 SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URI')
+SQLALCHEMY_POOL_RECYCLE = 299
 SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT')
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
